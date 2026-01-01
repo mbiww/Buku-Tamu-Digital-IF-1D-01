3f0f7eab2c3fcaf60cae2b68a8048bdcb83d4a73
mbiww <ajinabil207@gmail.com>
2026-01-01 21:59:45 +0700

    mengedit

--- data_tamu.php
+++ data_tamu.php
@@ -218,7 +218,6 @@ body {
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="dashboard_admin.php">Home</a></li>
                 <li class="nav-item"><a class="nav-link active" href="data_tamu.php">Data Tamu</a></li>
-                <li class="nav-item"><a class="nav-link" href="laporan.php">Laporan</a></li>
                 <li class="nav-item"><a class="nav-link" href="pengaturan_akun.php">Pengaturan Akun</a></li>
                 <li class="nav-item"><a class="nav-link text-danger" href="logout.php">Logout</a></li>
             </ul>
